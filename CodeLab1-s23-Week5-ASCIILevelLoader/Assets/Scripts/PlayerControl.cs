e0938f6d6eaf84c0c1aced68929caaf3a20d56bc
Matthew Parker <maddparker@gmail.com>
2023-02-26 20:59:22 -0500

    Removed Singleton Code from PlayerControl

--- CodeLab1-s23-Week5-ASCIILevelLoader/Assets/Scripts/PlayerControl.cs
+++ CodeLab1-s23-Week5-ASCIILevelLoader/Assets/Scripts/PlayerControl.cs
@@ -7,23 +7,7 @@ public class PlayerControl : MonoBehaviour
     public float forceAmount = 5;  //public var for force amount
     
     Rigidbody2D rb2D; //var for the Rigidbody2D
-
-    //static variable means the value is the same for all the objects of this class type and the class itself
-    public static PlayerControl instance; //this static var will hold the Singleton
-
-    void Awake()
-    {
-        if (instance == null)  //instance hasn't been set yet
-        {
-            DontDestroyOnLoad(gameObject);  //Dont Destroy this object when you load a new scene
-            instance = this; //set instance to this object
-        }
-        else //if the instance is already set to an object
-        {
-            Destroy(gameObject);  //destroy this new object, so there is only ever one
-        }
-    }
-
+    
 
     // Start is called before the first frame update
     void Start()
