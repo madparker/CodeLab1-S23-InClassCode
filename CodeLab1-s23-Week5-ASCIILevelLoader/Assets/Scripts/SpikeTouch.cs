11905e627625c3036bb70839f2b0ca7cead922e3
Matthew Parker <maddparker@gmail.com>
2023-02-27 14:00:31 -0500

    Added Doors, Spikes, and Levels

---
+++ CodeLab1-s23-Week5-ASCIILevelLoader/Assets/Scripts/SpikeTouch.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeTouch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("HIT: " + col.gameObject.name.Contains("Player"));
+        
+        if (col.gameObject.name.Contains("Player"))
+        {
+            GameManager.instance.GetComponent<ASCIILevelLoadScript>().ResetPlayer();
+        }
+    }
+}
